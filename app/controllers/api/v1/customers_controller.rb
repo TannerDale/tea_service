9e26b4844ada8e7517351eee6e7121f83eae3dab
Tanner Dale <tanner@tannerdale.com>
2022-01-03 16:53:41 -0600

    Add customer create actoin

--- app/controllers/api/v1/customers_controller.rb
+++ app/controllers/api/v1/customers_controller.rb
@@ -1,5 +1,11 @@
-class CustomersController < ApplicationController
+class Api::V1::CustomersController < ApplicationController
   def create
+    render json: Customer.create!(customer_params)
+  end
 
+  private
+
+  def customer_params
+    params.permit(:first_name, :last_name, :email, :address)
   end
 end
