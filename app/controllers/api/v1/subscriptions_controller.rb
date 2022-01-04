16c771b4c4d32f8e05ebb634ca8234358a14d37f
Tanner Dale <tanner@tannerdale.com>
2022-01-03 18:09:03 -0600

    Add subscription create action w/ tests

--- app/controllers/api/v1/subscriptions_controller.rb
+++ app/controllers/api/v1/subscriptions_controller.rb
@@ -1,7 +1,19 @@
 class Api::V1::SubscriptionsController < ApplicationController
   def index; end
 
-  def create; end
+  def create
+    render json: current_customer.subscriptions.create!(subscription_params)
+  end
 
   def destroy; end
+
+  private
+
+  def subscription_params
+    params.require(:subscription).permit(:title, :price, :status, :frequency, :tea_id)
+  end
+
+  def current_customer
+    Customer.find(params[:customer_id])
+  end
 end
--- spec/requests/v1/subscription_request_spec.rb
+++ spec/requests/v1/subscription_request_spec.rb
@@ -2,13 +2,70 @@ require 'rails_helper'
 
 describe 'Subscription Requests' do
   context 'POST v1/customers/:customer_id/subscriptions' do
+    let!(:customer) { create :customer }
+    let!(:tea) { create :tea }
+    let(:headers) { { 'CONTENT_TYPE': 'application/json' } }
+    let(:valid_params) do
+      {
+        title: 'My fav tea',
+        price: 1111,
+        status: 0,
+        frequency: 1,
+        tea_id: tea.id
+      }.to_json
+    end
+
     context 'with valid params' do
-      let!(:customer) { create :customer }
-      let!(:tea) { create :tea }
-      # let(:valid_params)
+      before { post api_v1_customer_subscriptions_path(customer.id), headers: headers, params: valid_params }
+
+      it 'creates the subscription' do
+        expect(Subscription.count).to eq(1)
+        expect(Customer.first.subscriptions.count).to eq(1)
+        expect(Tea.first.subscriptions.count).to eq(1)
+      end
+
+      it 'returns status code 200' do
+        expect(response).to have_http_status 200
+      end
     end
 
     context 'with invalid params' do
+      let(:invalid_tea) do
+        {
+          title: 'My fav tea',
+          price: 1111,
+          frequency: 1,
+          tea_id: tea.id + 1
+        }.to_json
+      end
+
+      context 'invalid tea_id' do
+        before { post api_v1_customer_subscriptions_path(customer.id), headers: headers, params: invalid_tea }
+
+        it 'does not create the subscription' do
+          expect(Subscription.count).to eq(0)
+          expect(Customer.first.subscriptions.count).to eq(0)
+          expect(Tea.first.subscriptions.count).to eq(0)
+        end
+
+        it 'returns status code 404' do
+          expect(response).to have_http_status 400
+        end
+      end
+
+      context 'invalid customer id' do
+        before { post api_v1_customer_subscriptions_path(customer.id + 1), headers: headers, params: valid_params }
+
+        it 'does not create the subscription' do
+          expect(Subscription.count).to eq(0)
+          expect(Customer.first.subscriptions.count).to eq(0)
+          expect(Tea.first.subscriptions.count).to eq(0)
+        end
+
+        it 'returns status code 404' do
+          expect(response).to have_http_status 404
+        end
+      end
     end
   end
 end
